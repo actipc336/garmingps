3af3daaef864c11e639403ee91e3e82d9d3060da
actipc336 <actipc336@gmail.com>
2025-06-24 15:05:21 +0530

    Update chat.js

--- docs/_static/chat.js
+++ docs/_static/chat.js
@@ -4,4 +4,3 @@ document.addEventListener("DOMContentLoaded", function () {
     ze.src = 'https://static.zdassets.com/ekr/snippet.js?key=7668c976-0fb7-47ee-8740-3e9115bdb10f';
     document.body.appendChild(ze);
 });
-
